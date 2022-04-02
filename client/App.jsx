adb4aa3cf14cf3eb6ebc1c294425ad5807a1fd50
Crawfish <nathanr.crawford39@gmail.com>
2022-04-02 12:11:44 -0400

    signup and login components

--- client/App.jsx
+++ client/App.jsx
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
-import Signup from './components/Signup.js'
+import { Signup, Login } from './components/index'
+import './styles/app.scss'
 
-export default class App extends Component {
-
-render() {   
-    return (
-        <div> 
-            <h1>App.jsx signup child</h1>
-            <Signup />
-        </div>
-        )
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogginActive: true
     };
+  }
+
+  componentDidMount() {
+    //Add .right by default
+    this.rightSide.classList.add("right");
+  }
+
+  changeState() {
+    const { isLogginActive } = this.state;
+
+    if (isLogginActive) {
+      this.rightSide.classList.remove("right");
+      this.rightSide.classList.add("left");
+    } else {
+      this.rightSide.classList.remove("left");
+      this.rightSide.classList.add("right");
+    }
+    this.setState(prevState => ({ isLogginActive: !prevState.isLogginActive }));
+  }
+
+  render() {
+    const { isLogginActive } = this.state;
+    const current = isLogginActive ? "Signup" : "Login";
+    const currentActive = isLogginActive ? "login" : "register";
+    return (
+      <div className="App">
+        <div className="login">
+          <div className="container" ref={ref => (this.container = ref)}>
+            {isLogginActive && (
+              <Login containerRef={ref => (this.current = ref)} />
+            )}
+            {!isLogginActive && (
+              <Signup containerRef={ref => (this.current = ref)} />
+            )}
+          </div>
+          <RightSide
+            current={current}
+            currentActive={currentActive}
+            containerRef={ref => (this.rightSide = ref)}
+            onClick={this.changeState.bind(this)}
+          />
+        </div>
+      </div>
+    );
+  }
 }
+
+const RightSide = props => {
+  return (
+    <div
+      className="right-side"
+      ref={props.containerRef}
+      onClick={props.onClick}
+    >
+      <div className="inner-container">
+        <div className="text">{props.current}</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
