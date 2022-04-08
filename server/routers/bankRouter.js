af7f521448e77f8b4be03ac63ac7ef3b734bea24
Javan Ang <javan.ang@outlook.com>
2022-04-07 20:21:01 -0500

    added add bank and delete bank feature

--- server/routers/bankRouter.js
+++ server/routers/bankRouter.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 const bankController = require('../controllers/bankController');
 
-router.get('/', bankController.getBankInfo, (req, res, next) => {
+router.get('/', bankController.getBankInfo, (req, res) => {
   return res.json(res.locals.data);
+});
+
+router.delete('/delete', bankController.deleteUserBank, (req, res) => {
+  return res.json('success');
 });
 
 module.exports = router;
