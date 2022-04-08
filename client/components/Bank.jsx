dd3558d71d6573f6bdfb6ca342e1386274cc70f0
Milos Popovic <milos_popovic@outlook.com>
2022-04-08 19:27:37 -0400

    Dashboard done

--- client/components/Bank.jsx
+++ client/components/Bank.jsx
@@ -1,61 +1,75 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import Collapsible from 'react-collapsible';
 import './bank.css';
 import { useNavigate } from 'react-router-dom';
 import PlaidLink from './PlaidLink'
 
+
 const Bank = (props) => {
+  const [bankInfo, setBankInfo] = useState(null);
+  let navigate = useNavigate();
 
-    let navigate = useNavigate();
+  const backToDashboard = (e) => {
+    e.preventDefault();
+    navigate('/dashboard')
+  }
 
-    const backToDashboard = (e) => {
-      e.preventDefault();
-      navigate('/dashboard')
+  useEffect(async () => {
+    const res = await fetch('/banks');
+    const data = await res.json();
+    console.log(data);
+    setBankInfo(...data);
+    console.log(bankInfo);
+    // fetch('/banks')
+    //   .then(res => res.json())
+    //   .then(data => {
+    //     console.log('data ', data);
+    //     setBankInfo(...data);
+    //   })
+    //   .then(console.log('bankInfo ', bankInfo))
+  }, []);
+
+  const [charities, setCharities] = useState([]);
+  
+  //make fetch request to pull charities data from db
+    useEffect (() => {
+      fetch('/banks')
+      .then(response => response.json())
+      .then(data => {
+        setBankInfo(...data);
+        });
+          },[]);
+
+
+  let banks;
+
+  useEffect(() => {
+    if (bankInfo) {
+      banks = bankInfo.map(el => {
+        <Collapsible trigger={`${el.bank_name}`}>
+          <div className="pTag">
+            <h4>{el.official_name}</h4>
+            <div>
+              <h4 className="accNumber">Account number:</h4> <p className="accNumber">{el.account_number}</p>
+            </div>
+            <br></br>
+            <button className="buttonNav">donate</button>
+          </div>
+        </Collapsible>
+      });
     }
+  }, [bankInfo]);
 
-    return (
-<<<<<<< HEAD
-      <div>
-        <button className='backtoDashboard' onClick={backToDashboard}>Back To Dashboard</button>
-        <PlaidLink></PlaidLink>
-=======
-      <div  className='backtoDashboard' >
-        <button onClick={backToDashboard}  className="buttonNav">dashboard</button>
->>>>>>> jwt
-        <Collapsible trigger="Bofa 2748">
-          <div className="pTag">
-              <h3>Bank of America</h3>
-              <div>
-              <h4 className="accNumber">Account number:</h4> <p className="accNumber">263759302632748</p>
-              </div>
-              <br></br>
-              <button className="buttonNav">donate</button>
-          </div>
-        </Collapsible>
-        <Collapsible trigger="Amex 2472">
-          <div className="pTag">
-              <h3>American Express</h3>
-              <div>
-              <h4 className="accNumber">Account number:</h4> <p className="accNumber">263759302632472</p>
-              </div>
-              <br></br>
-              <button className="button updateButton">donate</button>
-          </div>
-        </Collapsible>
-        <Collapsible trigger="Chase 2201">
-          <div className="pTag">
-              <h3>Chase Banking</h3>
-              <div>
-              <h4 className="accNumber">Account number:</h4> <p className="accNumber">263759302632201</p>
-              </div>
-              <br></br>
-              <button className="button updateButton">donate</button>
-          </div>
-        </Collapsible>
-      </div>
-    )
-}   
+  return (
+    <div className='backtoDashboard' >
+
+      <button onClick={backToDashboard} className="buttonNav">dashboard</button>
+      <PlaidLink></PlaidLink>
+      {banks}
+    </div>
+  )
+}
 
 
 export default Bank;
